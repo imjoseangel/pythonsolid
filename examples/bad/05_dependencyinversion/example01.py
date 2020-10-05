8da28896bc201c004347ca8aa3e7a0ccd5583f51
imjoseangel <josea.munoz@gmail.com>
2020-10-05 19:48:07 +0200

    Add DIP

---
+++ examples/bad/05_dependencyinversion/example01.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+DEFAULT_LION_LEG_COUNT = 4
+DEFAULT_MOUSE_LEG_COUNT = 4
+DEFAULT_SNAKE_LEG_COUNT = 0
+
+
+class Lion():
+
+    def get_legs_count(self):
+        print(DEFAULT_LION_LEG_COUNT)
+
+
+class Mouse():
+
+    def get_legs_count(self):
+        print(DEFAULT_MOUSE_LEG_COUNT)
+
+
+class Manager():
+
+    def __init__(self):
+        self.animal = None
+
+    def set_animal(self, animal):
+        assert isinstance(
+            animal, Lion), "`animal` must be of type {}".format(Lion)
+
+        self.animal = animal
+
+    def get_legs_count(self):
+        if self.animal is not None:
+            self.animal.get_legs_count()
+            # And some complex codes go here....
+
+
+def main():
+
+    lion = Lion()
+    manager = Manager()
+    manager.set_animal(lion)
+    manager.get_legs_count()
+
+    # The following will not work...
+    mouse = Mouse()
+    try:
+        manager.set_animal(mouse)
+    except AssertionError:
+        print("Manager fails to support mouse...")
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/good/05_dependencyinversion/example01.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+DEFAULT_LION_LEG_COUNT = 4
+DEFAULT_MOUSE_LEG_COUNT = 4
+DEFAULT_SNAKE_LEG_COUNT = 0
+
+
+class Animal():
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def get_legs_count(self):
+        pass
+
+
+class Lion(Animal):
+
+    def get_legs_count(self):
+        print(DEFAULT_LION_LEG_COUNT)
+
+
+class Mouse(Animal):
+
+    def get_legs_count(self):
+        print(DEFAULT_MOUSE_LEG_COUNT)
+
+
+class Snake(Animal):
+
+    def get_legs_count(self):
+        print(DEFAULT_SNAKE_LEG_COUNT)
+
+
+class Manager():
+
+    def __init__(self):
+        self.animal = None
+
+    def set_animal(self, animal):
+        assert isinstance(
+            animal, Animal), "`animal` must be of type {}".format(Animal)
+
+        self.animal = animal
+
+    def get_legs_count(self):
+        if self.animal is not None:
+            self.animal.get_legs_count()
+
+
+def main():
+
+    lion = Lion()
+    manager = Manager()
+    manager.set_animal(lion)
+    manager.get_legs_count()
+
+    # The following will work!
+    mouse = Mouse()
+    try:
+        manager.set_animal(mouse)
+        manager.get_legs_count()
+    except AssertionError:
+        print("Manager fails to support mouse...")
+
+
+if __name__ == '__main__':
+    main()
